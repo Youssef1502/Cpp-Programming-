2bae470812c6c54e475b7c22ce885f796de10160
Youssef Adel <youssef.adel.elawady@gmail.com>
2024-07-14 23:59:40 +0300

    Cpp program to describe a data type printing trick

---
+++ Basic/Cpp_DataType_Trick.cpp
@@ -0,0 +1,15 @@
+/**================================================================================
+--    Author    :   			YOUSSEF ADEL YOUSSEF
+
+-- Description  :   Cpp program to describe a data type printing trick.
+================================================================================**/
+
+#include<iostream>
+
+int main() 
+{
+    // you should use short, int and long only to prevent this error
+    uint8_t x = 97;
+    std::cout << "The x value =  " << x << std::endl;
+    return 0;
+}
